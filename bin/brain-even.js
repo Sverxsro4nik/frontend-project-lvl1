25c104615726f771294d3005f1a99c520d9ab54c
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-06 22:00:40 +0500

    Создание логики игры

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,52 @@
 import readlineSync from 'readline-sync';
 
-const Greeting = () => {
-  console.log('Welcome to the Brain Games!');
-};
-
 const answerName = () => {
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
 };
 
-const checkParity = () => {
-  Greeting();
-  answerName();
+const randomNum = () => {
+  const number = Math.round(Math.random() * 100);
+  return number;
 };
+
+const isEven = (number) => number % 2 === 0;
+
+const answerEvenNumber = (number) => {
+  const userAnswer = readlineSync.question(`Question: ${number}`);
+  console.log(`Your answer:\n ${userAnswer}`);
+  return userAnswer;
+};
+
+const wrongAnswer = (str) => {
+  if (str === 'yes') {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
+  } else {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
+  }
+};
+
+const checkParity = () => {
+  let result = 0;
+  console.log('Welcome to the Brain Games!');
+  answerName();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const number = randomNum();
+    const answer = answerEvenNumber(number);
+    if (isEven(number) && answer === 'yes') {
+      result += 1;
+    } else if (!isEven(number) && answer === 'no') {
+      result += 1;
+    } else {
+      // eslint-disable-next-line no-unused-expressions
+      answer === 'yes' ? wrongAnswer(answer) : wrongAnswer(answer);
+      break;
+    }
+  }
+  if (result === 3) {
+    console.log('Correct');
+  }
+};
+
+checkParity();
