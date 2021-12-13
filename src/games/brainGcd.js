42074aa6ea8a166911f78bfcb7bdb6eca07b5399
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-13 22:01:47 +0500

    изменение вывода при равных числах

--- src/games/brainGcd.js
+++ src/games/brainGcd.js
@@ -5,9 +5,8 @@ const greatestCommonFactor = (first, second) => {
   const bigger = largestNum(randomNum(first, second), randomNum(first, second));
   const smaller = smallerNum(randomNum(first, second), randomNum(first, second));
   const answer = `${smaller} ${bigger}`;
-  console.log(answer);
   let factor = 1;
-  if (bigger % smaller === 0) return smaller;
+  if (bigger % smaller === 0) return [smaller, answer];
   for (let i = 1; i <= smaller; i += 1) {
     if (smaller % i === 0 && bigger % i === 0) {
       factor = i;
