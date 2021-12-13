b8b4b610926b7ca15ead73728e0c095c1d3a4bfd
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-13 20:16:29 +0500

    Рефакторинг, перенос функций поиска оператора и калькулятора в логику игры

--- src/findOperator.js
+++
@@ -1,8 +0,0 @@
-import { randomNum } from './mathematics.js';
-
-export default () => {
-  const operator = ['+', '-', '*'];
-  const endOfArr = operator.length - 1;
-  const position = randomNum(0, endOfArr);
-  return operator[position];
-};
--- src/games/brainCalculator.js
+++ src/games/brainCalculator.js
@@ -1,5 +1,35 @@
 import gameLogic from '../index.js';
-import calculator from '../calculator.js';
+import randomNum from '../mathematics.js';
+
+// Получаем произвольный оператор
+const getOperator = () => {
+  const operator = ['+', '-', '*'];
+  const endOfArr = operator.length - 1;
+  const position = randomNum(0, endOfArr);
+  return operator[position];
+};
+
+// Выполняем простейшие математические операторы
+const calculator = (firstParam, secondParam) => {
+  const firstNum = randomNum(firstParam, secondParam);
+  const secondNum = randomNum(firstParam, secondParam);
+  let answer = 0;
+  const operator = getOperator();
+  switch (operator) {
+    case '+':
+      answer = firstNum + secondNum;
+      break;
+    case '-':
+      answer = firstNum - secondNum;
+      break;
+    case '*':
+      answer = firstNum * secondNum;
+      break;
+    default:
+      break;
+  }
+  return [answer, `${firstNum} ${operator} ${secondNum}`];
+};
 
 const brainCalculator = () => {
   gameLogic('What is the result of the expression?', 3, calculator, [1, 25]);
