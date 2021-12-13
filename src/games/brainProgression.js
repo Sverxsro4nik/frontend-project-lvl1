961c7e4e0c5969cfa314425d572e6663eda4d0c9
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-13 20:18:51 +0500

    Перенос функции создания прогрессии в файл с игрой, удаление файла createProgression

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -1,5 +1,18 @@
-import createProgression from '../createProgression.js';
 import gameLogic from '../index.js';
+import { randomNum } from '../mathematics.js';
+
+const createProgression = (startNum, endNum, stepStart, indexStart) => {
+  const step = randomNum(stepStart, endNum);
+  const firstNum = randomNum(startNum, endNum);
+  const progressionArr = [];
+  const randomIndex = randomNum(indexStart, progressionArr.length);
+  for (let i = firstNum, j = 0; j < 10; j += 1, i += step) {
+    progressionArr.push(i);
+  }
+  const result = progressionArr[randomIndex];
+  progressionArr[randomIndex] = '..';
+  return [result, progressionArr.join(' ')];
+};
 
 const brainProgression = () => {
   gameLogic(
