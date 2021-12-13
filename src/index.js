25eaab2977342f90795147e86909b1dc4a172fe8
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-13 20:35:00 +0500

    устранение ощибок линтера

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import askAnswer from './askAnswer.js';
 import wrongAnswer from './wrongAnswer.js';
 import userCongratilation from './userCongratilation.js';
 import correctAnswer from './correctAnswer.js';
+import printMessage from './printMessage.js';
 
 const gameLogic = (
   rulesOfTheGame,
@@ -11,7 +12,7 @@ const gameLogic = (
   paramGame,
 ) => {
   const userName = askName();
-  explainTask(rulesOfTheGame);
+  printMessage(rulesOfTheGame);
   let attempt = 0;
   for (let i = 0; i < numberOfAttempts; i += 1) {
     const [result, answer] = gameFunction(...paramGame);
