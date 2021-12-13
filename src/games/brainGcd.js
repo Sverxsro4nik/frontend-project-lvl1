44911c045bc7548fdb559f7d13d39c5b6b262904
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-13 20:21:19 +0500

    перенос тела функции поиска общего делителя в файл с игрой, удаление неиспользуемого файла

--- src/games/brainGcd.js
+++ src/games/brainGcd.js
@@ -1,5 +1,20 @@
 import gameLogic from '../index.js';
-import greatestCommonFactor from '../greatestCommonFactor.js';
+import { randomNum, largestNum, smallerNum } from '../mathematics.js';
+
+const greatestCommonFactor = (first, second) => {
+  const firstNumber = randomNum(first, second);
+  const secondNum = randomNum(first, second);
+  const bigger = largestNum(firstNumber, secondNum);
+  const smaller = smallerNum(firstNumber, secondNum);
+  let factor = 1;
+  if (bigger % smaller === 0) return smaller;
+  for (let i = 1; i <= smaller; i += 1) {
+    if (smaller % i === 0 && bigger % i === 0) {
+      factor = i;
+    }
+  }
+  return [factor, `${firstNumber} ${secondNum}`];
+};
 
 const brainGcd = () => {
   gameLogic(
