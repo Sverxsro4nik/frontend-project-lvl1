4134a847b1a94f4569d7c58ebccec3c9a35c7351
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2021-12-15 15:54:20 +0500

    Изменение поведения isEven, создание функции приводящей булевое значение в строковые варианты

--- src/games/brainCheckParity.js
+++ src/games/brainCheckParity.js
@@ -6,11 +6,17 @@ const rulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no
 // Проверка числа на четность
 const isEven = (start, end) => {
   const number = randomNum(start, end);
-  return [number % 2 === 0 ? 'yes' : 'no', `${number}`];
+  return [number % 2 === 0, number];
+};
+
+const checkTheAnswer = (start, end) => {
+  const [result, number] = isEven(start, end);
+  if (result) return ['yes', `${number}`];
+  return ['no', `${number}`];
 };
 
 const brainCheckParity = () => {
-  gameLogic(rulesOfTheGame, isEven);
+  gameLogic(rulesOfTheGame, checkTheAnswer);
 };
 
 export default brainCheckParity;
